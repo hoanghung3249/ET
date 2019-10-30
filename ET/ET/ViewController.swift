50f17cbc90098c123dcb0998257fd659b7fbecf5
HungNguyen <nguyenhung5890@gmail.com>
2019-10-30 14:07:05 +0700

    init project

---
+++ ET/ET/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ET
+//
+//  Created by HungNguyen on 10/30/19.
+//  Copyright © 2019 HungNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
